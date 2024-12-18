383f435cfe1a8d133b21643f4f4c81b2d23f15dd
Hackman <wilfreednouame@gmail.com>
2024-12-18 13:44:11 +0100

    avant optimisation

--- src/main/java/com/arabicRoman/ArabicRomanNumerals.java
+++ src/main/java/com/arabicRoman/ArabicRomanNumerals.java
@@ -2,9 +2,21 @@ package com.arabicRoman;
 
 public class ArabicRomanNumerals {
     public static String convert(int i) {
-        String result = "I";
-        if(i>1){
-            result = "II";
+        String result ="";
+        if(i<=3)
+        switch (i) {
+            case 1: result = "I";
+            break;
+              case 2: result = "II";
+              break;
+                case 3: result = "III";
+                break;
+                default: result = "I";
+        }
+
+        else {
+            result = "IV";
+            if(i>4) result="V";
         }
         return result;
     }
--- src/test/java/com/arabicRoman/ArabicRomanNumeralsTest.java
+++ src/test/java/com/arabicRoman/ArabicRomanNumeralsTest.java
@@ -1,37 +1,76 @@
 package com.arabicRoman;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ArabicRomanNumeralsTest {
-
+    ArabicRomanNumerals actual ;
+    String expected;
+     @BeforeEach
+     public void setUp() {
+        actual = new ArabicRomanNumerals();
+     }
+     @AfterEach
+     void tearDown() {
+         actual=null;
+         expected=null;
+     }
     @Test
     public void devrait_retourner_I() {
         //given
         String expected = "I";
         //when
-        String actual= ArabicRomanNumerals.convert(1);
+        String actual_value= actual.convert(1);
         //then
-        assertThat(actual).isEqualTo(expected);
+        assertThat(actual_value).isEqualTo(expected);
     }
     @Test
     public void devrait_retourner_II() {
         //given
         String expected = "II";
         //when
-        String actual= ArabicRomanNumerals.convert(2);
+        String actual_value= actual.convert(2);
         //then
-        assertThat(actual).isEqualTo(expected);
+        assertThat(actual_value).isEqualTo(expected);
     }
 
     @Test
     public void devrait_retourner_III() {
         //given
-        String expected = "I";
+        String expected = "III";
         //when
-        String actual= ArabicRomanNumerals.convert(1);
+        String actual_value= actual.convert(3);
         //then
-        assertThat(actual).isEqualTo(expected);
+        assertThat(actual_value).isEqualTo(expected);
+    }
+    @Test
+    public void devrait_retourner_IV() {
+        //given
+        String expected = "IV";
+        //when
+        String actual_value= actual.convert(4);
+        //then
+        assertThat(actual_value).isEqualTo(expected);
+    }
+    @Test
+    public void devrait_retourner_V() {
+        //given
+        String expected = "V";
+        //when
+        String actual_value= actual.convert(5);
+        //then
+        assertThat(actual_value).isEqualTo(expected);
+    }
+    @Test
+    public void devrait_retourner_VI() {
+        //given
+        String expected = "VI";
+        //when
+        String actual_value= actual.convert(6);
+        //then
+        assertThat(actual_value).isEqualTo(expected);
     }
 }
