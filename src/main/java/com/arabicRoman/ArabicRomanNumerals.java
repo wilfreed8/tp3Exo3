7698b0c5d02c55c9904be8fbe282fd152a530758
Hackman <wilfreednouame@gmail.com>
2024-12-18 22:02:13 +0100

    fin

--- src/main/java/com/arabicRoman/ArabicRomanNumerals.java
+++ src/main/java/com/arabicRoman/ArabicRomanNumerals.java
@@ -1,23 +1,20 @@
 package com.arabicRoman;
 
 public class ArabicRomanNumerals {
-    public static String convert(int i) {
-        String result ="";
-        if(i<=3)
-        switch (i) {
-            case 1: result = "I";
-            break;
-              case 2: result = "II";
-              break;
-                case 3: result = "III";
-                break;
-                default: result = "I";
+    public static String convert(int nbr) {
+        int[] values = { 50,40,10,9,5, 4, 1};
+        String[] symbols = {"L","XL","X","IX","V", "IV", "I"};
+
+        StringBuilder roman = new StringBuilder();
+
+        for (int i = 0; i < values.length; i++) {
+            while (nbr >= values[i]) { // Tant que le nombre peut être réduit
+                roman.append(symbols[i]);
+                nbr -= values[i];
+            }
         }
 
-        else {
-            result = "IV";
-            if(i>4) result="V";
-        }
-        return result;
+        return roman.toString();
+
     }
 }
--- src/test/java/com/arabicRoman/ArabicRomanNumeralsTest.java
+++ src/test/java/com/arabicRoman/ArabicRomanNumeralsTest.java
@@ -73,4 +73,122 @@ public class ArabicRomanNumeralsTest {
         //then
         assertThat(actual_value).isEqualTo(expected);
     }
+    @Test
+    public void devrait_retourner_VII() {
+        //given
+        String expected = "VII";
+        //when
+        String actual_value= actual.convert(7);
+        //then
+        assertThat(actual_value).isEqualTo(expected);
+    }
+    @Test
+    public void devrait_retourner_VIII() {
+        //given
+        String expected = "VIII";
+        //when
+        String actual_value= actual.convert(8);
+        //then
+        assertThat(actual_value).isEqualTo(expected);
+    }
+    @Test
+    public void devrait_retourner_IX() {
+        //given
+        String expected = "IX";
+        //when
+        String actual_value= actual.convert(9);
+        //then
+        assertThat(actual_value).isEqualTo(expected);
+    }
+    @Test
+    public void devrait_retourner_X() {
+        //given
+        String expected = "X";
+        //when
+        String actual_value= actual.convert(10);
+        //then
+        assertThat(actual_value).isEqualTo(expected);
+    }
+    @Test
+    public void devrait_retourner_XI() {
+        //given
+        String expected = "XI";
+        //when
+        String actual_value= actual.convert(11);
+        //then
+        assertThat(actual_value).isEqualTo(expected);
+    }
+    @Test
+    public void devrait_retourner_XIV() {
+        //given
+        String expected = "XIV";
+        //when
+        String actual_value= actual.convert(14);
+        //then
+        assertThat(actual_value).isEqualTo(expected);
+    }
+    @Test
+    public void devrait_retourner_XV() {
+        //given
+        String expected = "XV";
+        //when
+        String actual_value= actual.convert(15);
+        //then
+        assertThat(actual_value).isEqualTo(expected);
+    }
+    @Test
+    public void devrait_retourner_XIX() {
+        //given
+        String expected = "XIX";
+        //when
+        String actual_value= actual.convert(19);
+        //then
+        assertThat(actual_value).isEqualTo(expected);
+    }
+    @Test
+    public void devrait_retourner_XX() {
+        //given
+        String expected = "XX";
+        //when
+        String actual_value= actual.convert(20);
+        //then
+        assertThat(actual_value).isEqualTo(expected);
+    }
+    @Test
+    public void devrait_retourner_XXX() {
+        //given
+        String expected = "XXX";
+        //when
+        String actual_value= actual.convert(30);
+        //then
+        assertThat(actual_value).isEqualTo(expected);
+    }
+    @Test
+    public void devrait_retourner_XL() {
+        //given
+        String expected = "XL";
+        //when
+        String actual_value= actual.convert(40);
+        //then
+        assertThat(actual_value).isEqualTo(expected);
+    }
+    @Test
+    public void devrait_retourner_XLV() {
+        //given
+        String expected = "XLV";
+        //when
+        String actual_value= actual.convert(45);
+        //then
+        assertThat(actual_value).isEqualTo(expected);
+    }
+    @Test
+    public void devrait_retourner_L() {
+        //given
+        String expected = "L";
+        //when
+        String actual_value= actual.convert(50);
+        //then
+        assertThat(actual_value).isEqualTo(expected);
+    }
+
 }
