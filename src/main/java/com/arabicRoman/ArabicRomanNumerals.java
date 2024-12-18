5aee593cf1a489db4b2424875875224378252758
Hackman <wilfreednouame@gmail.com>
2024-12-18 13:05:16 +0100

    test de III

--- src/main/java/com/arabicRoman/ArabicRomanNumerals.java
+++ src/main/java/com/arabicRoman/ArabicRomanNumerals.java
@@ -1,4 +1,11 @@
 package com.arabicRoman;
 
 public class ArabicRomanNumerals {
+    public static String convert(int i) {
+        String result = "I";
+        if(i>1){
+            result = "II";
+        }
+        return result;
+    }
 }
--- src/test/java/com/arabicRoman/ArabicRomanNumeralsTest.java
+++ src/test/java/com/arabicRoman/ArabicRomanNumeralsTest.java
@@ -1,4 +1,37 @@
 package com.arabicRoman;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class ArabicRomanNumeralsTest {
+
+    @Test
+    public void devrait_retourner_I() {
+        //given
+        String expected = "I";
+        //when
+        String actual= ArabicRomanNumerals.convert(1);
+        //then
+        assertThat(actual).isEqualTo(expected);
+    }
+    @Test
+    public void devrait_retourner_II() {
+        //given
+        String expected = "II";
+        //when
+        String actual= ArabicRomanNumerals.convert(2);
+        //then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void devrait_retourner_III() {
+        //given
+        String expected = "I";
+        //when
+        String actual= ArabicRomanNumerals.convert(1);
+        //then
+        assertThat(actual).isEqualTo(expected);
+    }
 }
